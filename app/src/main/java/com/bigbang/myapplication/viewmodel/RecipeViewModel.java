13779f1d0488c9c40ff8972db6121a90c9f62584
danezorrilla <danezorrilla@gmail.com>
2020-04-15 09:32:35 -0400

    implement Firebase Event

--- app/src/main/java/com/bigbang/myapplication/viewmodel/RecipeViewModel.java
+++ app/src/main/java/com/bigbang/myapplication/viewmodel/RecipeViewModel.java
@@ -5,8 +5,45 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.bigbang.myapplication.model.Recipe;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+
 public class RecipeViewModel extends AndroidViewModel {
+
+    private PublishSubject<List<Recipe>> recipePublishSubject = PublishSubject.create();
+
     public RecipeViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public Observable<List<Recipe>> getRecipes(){
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("recipes");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Recipe> recipeList = new ArrayList<>();
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    recipeList.add(ds.getValue(Recipe.class));
+                }
+                recipePublishSubject.onNext(recipeList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return recipePublishSubject;
     }
 }
